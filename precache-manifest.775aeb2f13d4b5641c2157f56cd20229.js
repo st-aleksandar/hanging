67ee1d1ec328d5cf90c8fa1d5ab15c98a149a481
Aleksandar Stojanovic <aca_fpi@hotmail.com>
2019-02-13 09:51:15 +0100

    Updates

---
+++ precache-manifest.775aeb2f13d4b5641c2157f56cd20229.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10f6bd2a1509d69040",
+    "url": "/hanging/static/js/runtime~main.ab10f6bd.js"
+  },
+  {
+    "revision": "0333d04c3dacac1e369f",
+    "url": "/hanging/static/js/main.0333d04c.chunk.js"
+  },
+  {
+    "revision": "464e95bb0b06898eeec8",
+    "url": "/hanging/static/js/2.464e95bb.chunk.js"
+  },
+  {
+    "revision": "0333d04c3dacac1e369f",
+    "url": "/hanging/static/css/main.51c73473.chunk.css"
+  },
+  {
+    "revision": "0d332315437e2cc25c60822705e74b02",
+    "url": "/hanging/index.html"
+  }
+];
